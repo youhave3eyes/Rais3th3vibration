5717311086f347abd5d2a7aca508a550f419119b
youhave3eyes <youhave3eyes@users.noreply.github.com>
2026-01-20 13:17:28 -0600

    Update config for Rais3th3vibration repository

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rtv33-react/',
+  base: '/Rais3th3vibration/',
   plugins: [react()],
   server: {
     port: 4747,
